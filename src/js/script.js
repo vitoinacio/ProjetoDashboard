2903d2c16424a5681e53d5f680d15925918da5b8
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-09-09 13:19:03 -0300

    menu mobile adicionado a responsividade, home page tortalmente responsiva

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 import graficoCircular from "./modules/graficoCircular.js";
 import graficoDeBarra from "./modules/graficoDeBarra.js";
+import menuMobile from "./modules/menuMobile.js";
 
 
 graficoDeBarra();
 graficoCircular();
+menuMobile();
