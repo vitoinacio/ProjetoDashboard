1c12db6837eec6a6f206e1f8dad4f65bb7d311b0
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-09-18 13:53:39 -0300

    adicionei o evento de clique para o botao entrar do menu mobile e ajustei a responsividade do container

--- src/js/modules/login.js
+++ src/js/modules/login.js
@@ -1,15 +1,19 @@
 export default function login_btn() {
-    
+  const buttons = document.querySelectorAll("#loginBtn");
 
-    document.getElementById("loginBtn").addEventListener("click", function() {
-        document.getElementById("loginModal").style.display = "flex"
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      document.getElementById("loginModal").style.display = "flex";
     });
-    
-    document.getElementById("closeBtn").addEventListener("click", function() {
-        document.getElementById("loginModal").style.display = "none";
-    });
-    
-    window.addEventListener("click", function(event) {
-        if (event.target == document.getElementById("loginModal")) {
-            document.getElementById("loginModal").style.display = "none";
-    }})};
+  });
+
+  document.getElementById("closeBtn").addEventListener("click", function () {
+    document.getElementById("loginModal").style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == document.getElementById("loginModal")) {
+      document.getElementById("loginModal").style.display = "none";
+    }
+  });
+}
