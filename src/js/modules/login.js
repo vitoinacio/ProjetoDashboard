9764cee97e0d56c33318b1de4427949754568449
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-09-25 14:40:18 -0300

    tela cadastro iniciada

--- src/js/modules/login.js
+++ src/js/modules/login.js
@@ -1,19 +1,20 @@
 export default function login_btn() {
-  const buttons = document.querySelectorAll("#loginBtn");
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      document.getElementById("loginModal").style.display = "flex";
+  const buttons = document.querySelectorAll('#loginBtn');
+  if (buttons.length) {
+    buttons.forEach((button) => {
+      button.addEventListener('click', function () {
+        document.getElementById('loginModal').style.display = 'flex';
+      });
     });
-  });
 
-  document.getElementById("closeBtn").addEventListener("click", function () {
-    document.getElementById("loginModal").style.display = "none";
-  });
+    document.getElementById('closeBtn').addEventListener('click', function () {
+      document.getElementById('loginModal').style.display = 'none';
+    });
 
-  window.addEventListener("click", function (event) {
-    if (event.target == document.getElementById("loginModal")) {
-      document.getElementById("loginModal").style.display = "none";
-    }
-  });
+    window.addEventListener('click', function (event) {
+      if (event.target == document.getElementById('loginModal')) {
+        document.getElementById('loginModal').style.display = 'none';
+      }
+    });
+  }
 }
