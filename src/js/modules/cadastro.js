28807edd6fa6e6927b7a2e3bae838dd850733df2
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-10-02 01:20:15 -0300

    adicionado a validaçao dA Primeira parte do cadastro do usuario

--- src/js/modules/cadastro.js
+++ src/js/modules/cadastro.js
@@ -1,71 +1,74 @@
 const validateCadastro = () => {
-
-  const containerPessoais = document.querySelector(".infoPessoais") 
-  const containerAdicionais = document.querySelector(".infoAdicionais")
-  const form = document.querySelectorAll("form.formInfoPessoais");
-  const formAd = document.querySelectorAll("form.forminfoAdicionais");
+  const containerPessoais = document.querySelector('.infoPessoais');
+  const containerAdicionais = document.querySelector('.infoAdicionais');
+  const form = document.querySelectorAll('form.formInfoPessoais');
+  const formAd = document.querySelectorAll('form.forminfoAdicionais');
 
   if (form.length) {
-
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
-    const currentYear = +today.toLocaleDateString().split("/")[2]
+    const currentYear = +today.toLocaleDateString().split('/')[2];
 
-    const validityNasc = (event, avanco) =>{
-      const nasc = +form[0][2].value.split("-").reverse()[2]
-      if ((currentYear - nasc) < 16) {
+    const validityNasc = (event) => {
+      const nasc = +form[0][2].value.split('-').reverse()[2];
+      if (currentYear - nasc < 16) {
         event.preventDefault();
-        form[0][2].focus()
-        form[0][2].style.borderBottom = "1px solid red"
+        form[0][2].focus();
+        form[0][2].style.borderBottom = '1px solid red';
         // form[0][2].setCustomValidity("Precisa ser maior de 16 anos");
-      } else {
-        avanco = true
       }
-    }
+    };
 
     const emailMascara = () => {
       const re = /\S+@\S+\.\S+/;
-      if(re.test(form[0][3].value) === false){
-        form[0][3].setCustomValidity("email errado")
+      if (re.test(form[0][3].value) === false) {
+        form[0][3].setCustomValidity('email errado');
       } else {
-        form[0][3].setCustomValidity("")
+        form[0][3].setCustomValidity('');
       }
-    }
+    };
 
-    const validitySenha = (avanco) =>{
-      if (form[0][4].value !== form[0][5].value){
-        form[0][5].setCustomValidity("As senhas não coincidem")
+    const validitySenha = () => {
+      if (form[0][4].value !== form[0][5].value) {
+        form[0][5].setCustomValidity('As senhas não coincidem');
+        return false;
       } else {
-        form[0][5].setCustomValidity("")
-        return avanco = true
+        form[0][5].setCustomValidity('');
+        return true;
       }
-    }
+      return false;
+    };
 
-    const validityForm = () =>{
-      const avanco = false;
-      validityNasc(avanco);
-      validitySenha(avanco);
-      if (form[0][7].checked === true){
-        if (avanco === true) {
-          containerPessoais.display = "none"
-          containerAdicionais.display = "flex"
+    const validityForm = (event) => {
+      validityNasc();
+      validitySenha();
+      if (form[0][7].checked) {
+        if (
+          form[0][0].validity.valid &&
+          form[0][3].validity.valid &&
+          validitySenha()
+        ) {
+          event.preventDefault();
+          containerAdicionais.style.display = 'flex';
+          containerPessoais.style.display = 'none';
+          setTimeout(() => {
+            containerAdicionais.classList.add('active');
+          }, 300);
         }
       }
-    }
+    };
 
-    const mascaraForm = () =>{
+    const mascaraForm = () => {
       emailMascara();
-    }
+    };
 
-    const formArray = new Array(form[0])
-    console.log(formArray)
-    
+    const formArray = new Array(form[0]);
+
     formArray.forEach((element) => {
       element.addEventListener('input', mascaraForm);
     });
 
     form[0][6].addEventListener('click', validityForm);
-
   }
 };
 
