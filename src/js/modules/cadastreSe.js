6d2271397c0bacec07db2244eab3727380777540
Victor Hugo <victor.hugo.ina10@gmail.com>
2024-10-16 00:09:27 -0300

    update error index

--- src/js/modules/cadastreSe.js
+++ src/js/modules/cadastreSe.js
@@ -3,26 +3,67 @@ const validarEmail = (email) => {
   return re.test(email);
 };
 
+function setError(reference, text) {
+  // Obtém a posição do elemento de referência
+  const posi = reference.getBoundingClientRect();
+
+  // Cria o elemento de erro
+  const error = document.createElement('p');
+  error.classList.add('error');
+  error.innerText = text;
+  
+  // Define o estilo do elemento de erro
+  error.style.position = 'absolute';
+  error.style.color = 'red';
+  error.style.zIndex = '1';
+  error.style.fontSize = '10px';
+  error.style.top = `${reference.offsetTop + 60}px`;
+  error.style.left = `${Math.trunc(posi.x) - posi.x + 20}px`;
+
+  window.onresize = () => {
+    const posi = reference.getBoundingClientRect();
+    error.style.top = `${reference.offsetTop + 60}px`;
+    error.style.left = `${Math.trunc(posi.x)- posi.x + 20}px`;
+  }
+  // Insere o elemento de erro após o elemento de referência
+  reference.parentNode.insertBefore(error, reference.nextSibling);
+}
+
+const removeError = () => {
+  // Seleciona o elemento de erro
+  const error = document.querySelector('.error');
+
+  // Remove o elemento de erro se ele existir e for filho do elemento de referência
+  if (error && error.classList.contains('error')) {
+    error.parentNode.removeChild(error);
+  }
+};
+
 export default function cadastreSe() {
   const btn = document.querySelector('#btnCadastreSe');
   if (btn) {
     btn.addEventListener('click', () => {
       const email = document.querySelector('#emailIndex');
-      if (validarEmail(email.value)) {
+      if (email.value === '') {
+        removeError()
+        setError(email, 'Digite seu email');
+        email.focus();
+        email.style.border = '1px solid red'
+        email.addEventListener('input', ()=>{
+          removeError()
+          email.style.border = '1px solid var(--cor2)'
+        })
+      } else if (validarEmail(email.value)) {
         sessionStorage.setItem('emailIndex', email.value);
         window.location = 'src/pages/cadastro.html';
       } else {
-        const error = document.createElement('p')
-        error.textContent = 'Por favor, insira um email válido.'
-        error.style.color = 'red'
-        error.style.display = 'absolute'
-        error.style.top = '-110px'
-        error.style.fontSize = '0.8rem'
-        error.style.fontWeight = 'bold'
-        error.style.textAlign = 'center'
-        console.log(btn.previousElementSibling.appendChild(error))
+        removeError()
+        setError(email, 'Digite um email válido');
+        email.focus();
+        email.style.border = '1px solid red'
         email.addEventListener('input', ()=>{
-          error.remove()
+          removeError()
+          email.style.border = '1px solid var(--cor2)'
         })
       }
     });
