7f2840f0f75ea2bca50d192b2ee8bfab7278b2ae
Josué Almeida <jjj.josuealmeida@gmail.com>
2024-11-06 18:16:20 -0300

    Alteração no nome do planejamento.php
    Alteração no nome planejamento.php para processa_planejamento.php para melhor organização.

---
+++ src/php/processa_planejamento.php
@@ -0,0 +1,24 @@
+<?php
+require_once "conexao.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $ident_deb = htmlspecialchars(trim($_POST["ident_deb"]));
+    $obs_deb = htmlspecialchars(trim($_POST["obs_deb"]));
+    $valor_deb = str_replace(",", ".", trim(str_replace("R$", "", $_POST["valor_deb"])));
+    $data_venc = htmlspecialchars(trim($_POST["data_venc"]));
+    $notifi = intval(htmlspecialchars(trim($_POST["notficacao"])))
+;
+
+    $stmt = $conn->prepare("INSERT INTO debito (ident_deb, obs_deb, valor_deb, data_venc, notifi) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssdi", $ident_deb, $obs_deb, $valor_deb, $data_venc, $notifi);
+
+    if ($stmt->execute()) {
+        echo "Débito inseridos com sucesso!";
+    } else {
+        echo "Erro: " . $stmt->error;
+    }
+    $stmt->close();
+
+
+ 
+}
